20dfc1142bba20c66e6d68175fa5efb60d8d62d7
Cyrill Bolliger <mail@cyrill.me>
2024-04-18 23:50:35 +0200

    [FEAT] simplify spinner

---
+++ frontend/src/components/base/BaseSpinner.vue
@@ -0,0 +1,19 @@
+<template>
+  <figure class="column items-center">
+    <q-icon name="img:/icons/spinner.svg" :size="props.size" />
+    <figcaption class="text-muted">{{ label || t('base.loading') }}</figcaption>
+  </figure>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'src/composables/useI18n';
+import { QIconProps } from 'quasar';
+const { t } = useI18n();
+
+export interface BaseSpinnerProps {
+  size?: QIconProps['size'];
+  label?: string;
+}
+
+const props = defineProps<BaseSpinnerProps>();
+</script>

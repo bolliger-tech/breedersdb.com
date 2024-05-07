9c220d21f33f129e2951aaed108615dd3125870b
Cyrill Bolliger <mail@cyrill.me>
2024-05-07 11:38:46 +0200

    [FEAT] localized sort

---
+++ frontend/src/composables/localizedSort.ts
@@ -0,0 +1,24 @@
+import type { Locale } from 'vue-i18n';
+import { useI18n } from './useI18n';
+
+export function useLocalizedSort({
+  locale,
+  options,
+}: {
+  locale?: Locale;
+  options?: Intl.CollatorOptions;
+} = {}) {
+  const { locale: defaultLocale } = useI18n();
+  const collator = new Intl.Collator(locale || defaultLocale.value, {
+    numeric: true,
+    sensitivity: 'accent',
+    ignorePunctuation: true,
+    ...options,
+  });
+
+  return {
+    collator,
+    comparator: collator.compare,
+    localizedSort: (array: string[]): string[] => array.sort(collator.compare),
+  };
+}

c5eae5a37cea300693b911f6cbd5e18a89853b30
Cyrill Bolliger <mail@cyrill.me>
2024-05-27 18:13:12 +0200

    [FEAT] improved error message

--- frontend/src/components/base/BaseSuspense/BaseSuspenseError.vue
+++ frontend/src/components/base/BaseSuspense/BaseSuspenseError.vue
@@ -3,16 +3,16 @@
     <q-card-section>
       <div class="row items-center q-gutter-sm">
         <q-icon name="error" size="2em" />
-        <span class="text-h6">{{ t('base.suspenseWithError.title') }}</span>
+        <span class="text-h6">{{
+          error.name || t('base.suspenseWithError.title')
+        }}</span>
       </div>
     </q-card-section>
     <q-card-section class="q-pt-none">
-      <div v-if="'graphQLErrors' in error && error.graphQLErrors.length">
-        <div v-for="err in error.graphQLErrors" :key="err.message">
-          {{ err.message }}
-        </div>
+      <BaseGraphqlError v-if="combinedError" :error="combinedError" />
+      <div v-else>
+        {{ error.message }}
       </div>
-      <div v-else>{{ error.message }}</div>
     </q-card-section>
     <q-separator dark />
     <q-card-actions :align="'center'">
@@ -28,16 +28,24 @@
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue';
 import { CombinedError } from '@urql/vue';
 import { useI18n } from 'src/composables/useI18n';
+import BaseGraphqlError from '../BaseGraphqlError.vue';
+import { computed, onMounted } from 'vue';
 const { t } = useI18n();
 
-const { error } = defineProps({
-  error: {
-    type: Object as PropType<Error | CombinedError>,
-    required: true,
-  },
+export interface BaseSuspenseErrorProps {
+  error: Error | CombinedError;
+}
+
+const props = defineProps<BaseSuspenseErrorProps>();
+
+const combinedError = computed(() => {
+  return props.error instanceof CombinedError ? props.error : null;
+});
+
+onMounted(() => {
+  console.error(props.error);
 });
 
 function reloadWindow() {

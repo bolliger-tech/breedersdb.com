a4b91695a9ef6b1899f023e0bcf89402752ac276
Cyrill Bolliger <mail@cyrill.me>
2024-05-28 17:28:35 +0200

    [FEAT] remove name from attributions filter

--- frontend/src/components/query/Filter/ColumnDefinitions/useAttributionViewColumns.ts
+++ frontend/src/components/query/Filter/ColumnDefinitions/useAttributionViewColumns.ts
@@ -1,41 +1,29 @@
-import { useQuery } from '@urql/vue';
 import { FilterRuleType } from '../filterRule';
 import type { FilterColumnConstructorData } from './useColumnDefinitions';
-import { graphql } from 'src/graphql';
 import { FilterRuleColumn } from '../filterRuleColumn';
 import { uppercaseFirstLetter } from 'src/utils/stringUtils';
 import { useI18n } from 'src/composables/useI18n';
 import { computed, ref } from 'vue';
-
-const query = graphql(`
-  query Attributes {
-    attributes(where: { disabled: { _eq: false } }, order_by: { name: asc }) {
-      id
-      name
-    }
-  }
-`);
+import { BaseTable } from '../filterNode';
+import { useEntityName } from 'src/composables/useEntityName';
 
 export function useAttributionViewColumns() {
   const { t } = useI18n();
-  const pause = ref(true);
+  const { getEntityName } = useEntityName();
 
-  const { data, fetching, error } = useQuery({
-    query,
-    variables: undefined,
-    pause,
-  });
+  const data = ref<FilterColumnConstructorData[] | undefined>(undefined);
 
   const columns = computed(() => {
-    if (error.value || !data.value) {
+    if (!data.value) {
       return [];
     }
 
-    const attributeNames = data.value.attributes.map((a) => a.name);
-    const columnData = getColumnConstrutorData(attributeNames);
-    const tableLabel = t('filter.attribute');
+    const tableLabel = getEntityName({
+      table: BaseTable.Attributions,
+      capitalize: true,
+    });
 
-    return columnData.map((data) => {
+    return data.value.map((data) => {
       return new FilterRuleColumn({
         tableName: data.table,
         tableColumnName: data.column,
@@ -46,9 +34,12 @@ export function useAttributionViewColumns() {
     });
   });
 
+  const fetching = ref(false);
+  const error = ref(undefined);
+
   return {
     activate: () => {
-      pause.value = false;
+      data.value = columnData;
     },
     data: columns,
     fetching,
@@ -56,57 +47,41 @@ export function useAttributionViewColumns() {
   };
 }
 
-function getColumnConstrutorData(
-  attributeNames: string[],
-): FilterColumnConstructorData[] {
-  return [
-    {
-      table: 'attributions_view',
-      column: 'id',
-      labelKey: 'entity.id',
-      schema: {
-        type: FilterRuleType.Integer,
-        allowEmpty: false,
-        validation: {
-          min: 1,
-          max: Number.MAX_SAFE_INTEGER,
-          step: 1,
-        },
+const columnData: FilterColumnConstructorData[] = [
+  {
+    table: 'attributions_view',
+    column: 'id',
+    labelKey: 'entity.id',
+    schema: {
+      type: FilterRuleType.Integer,
+      allowEmpty: false,
+      validation: {
+        min: 1,
+        max: Number.MAX_SAFE_INTEGER,
+        step: 1,
       },
     },
-    {
-      table: 'attributions_view',
-      column: 'attribute_name',
-      labelKey: 'entity.name',
-      schema: {
-        type: FilterRuleType.Enum,
-        allowEmpty: false,
-        validation: {
-          options: attributeNames,
-        },
+  },
+  {
+    table: 'attributions_view',
+    column: 'note',
+    labelKey: 'entity.note',
+    schema: {
+      type: FilterRuleType.String,
+      allowEmpty: true,
+      validation: {
+        maxLen: 2047,
+        pattern: null,
       },
     },
-    {
-      table: 'attributions_view',
-      column: 'note',
-      labelKey: 'entity.note',
-      schema: {
-        type: FilterRuleType.String,
-        allowEmpty: true,
-        validation: {
-          maxLen: 2047,
-          pattern: null,
-        },
-      },
+  },
+  {
+    table: 'attributions_view',
+    column: 'exceptional_attribution',
+    labelKey: 'attributions.columns.exceptional_attribution',
+    schema: {
+      type: FilterRuleType.Boolean,
+      allowEmpty: false,
     },
-    {
-      table: 'attributions_view',
-      column: 'exceptional_attribution',
-      labelKey: 'attributions.columns.exceptional_attribution',
-      schema: {
-        type: FilterRuleType.Boolean,
-        allowEmpty: false,
-      },
-    },
-  ];
-}
+  },
+];

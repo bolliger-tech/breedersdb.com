2c6546c661c45bf5e2776a7dfb49a3d67a31f567
Cyrill Bolliger <mail@cyrill.me>
2024-06-13 18:05:03 +0200

    [TEST] plant groups entity

--- backend/tests/fetch.js
+++ backend/tests/fetch.js
@@ -15,3 +15,23 @@ export async function post(query) {
   });
   return resp.json();
 }
+
+export async function postOrFail(query) {
+  const resp = await fetch(apiUrl, {
+    method: 'POST',
+    headers: defaultHeaders,
+    body: JSON.stringify(query),
+  });
+
+  const json = await resp.json();
+
+  if (json.errors) {
+    if (json.errors[0].extensions?.internal?.error) {
+      console.error(json.errors[0].extensions.internal.error);
+    } else {
+      console.log(json.errors[0]);
+    }
+  }
+
+  return json;
+}

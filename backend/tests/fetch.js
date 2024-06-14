2e5b245ad3b07bf5ae4bf6990d2b5675c7c4da9c
Cyrill Bolliger <mail@cyrill.me>
2024-06-14 14:22:45 +0200

    [FIX] postOrFail not throwing

--- backend/tests/fetch.js
+++ backend/tests/fetch.js
@@ -26,11 +26,9 @@ export async function postOrFail(query) {
   const json = await resp.json();
 
   if (json.errors) {
-    if (json.errors[0].extensions?.internal?.error) {
-      console.error(json.errors[0].extensions.internal.error);
-    } else {
-      console.log(json.errors[0]);
-    }
+    const error =
+      json.errors[0].extensions?.internal?.error ?? json.errors[0].message;
+    throw new Error(JSON.stringify(error));
   }
 
   return json;

84a9c59490552b0d80104aa4782af759cb895677
patte <p@tte.io>
2024-06-21 13:48:43 +0200

    [FEAT] tests for auth

--- backend/tests/fetch.ts
+++ backend/tests/fetch.ts
@@ -6,16 +6,7 @@ const defaultHeaders = {
   'Content-Type': 'application/json',
 };
 
-export async function post(query: any, headers: HeadersInit = defaultHeaders) {
-  const resp = await fetch(config.HASURA_GRAPHQL_URL, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(query),
-  });
-  return resp.json();
-}
-
-export async function postOrFail(
+export async function postRaw(
   query: any,
   headers: HeadersInit = defaultHeaders,
 ) {
@@ -24,14 +15,34 @@ export async function postOrFail(
     headers,
     body: JSON.stringify(query),
   });
+  return { json: await resp.json(), resp };
+}
 
-  const json = await resp.json();
+export async function post(query: any, headers: HeadersInit = defaultHeaders) {
+  const { json } = await postRaw(query, headers);
+  return json;
+}
+
+export async function postOrFailRaw(
+  query: any,
+  headers: HeadersInit = defaultHeaders,
+) {
+  const { json, resp } = await postRaw(query, headers);
 
   if (json.errors) {
     const error =
       json.errors[0].extensions?.internal?.error ?? json.errors[0].message;
     throw new Error(JSON.stringify(error));
   }
+
+  return { json, resp };
+}
+
+export async function postOrFail(
+  query: any,
+  headers: HeadersInit = defaultHeaders,
+) {
+  const { json } = await postOrFailRaw(query, headers);
 
   return json;
 }

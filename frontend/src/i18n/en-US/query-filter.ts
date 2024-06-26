fd59546017ac434ae09c13e100bd16ef6f9f7d26
Cyrill Bolliger <mail@cyrill.me>
2024-06-26 18:23:30 +0200

    [REFACTOR] reuse noResults string

--- frontend/src/i18n/en-US/query-filter.ts
+++ frontend/src/i18n/en-US/query-filter.ts
@@ -38,8 +38,6 @@ export const filter = {
   withNoAttributions:
     'Include {entities} without {attributeName} attributions.',
 
-  noResults: 'No results.',
-
   operators: {
     equals: 'equals',
     notEquals: 'not equals',
@@ -67,73 +65,3 @@ export const filter = {
     or: 'or',
   },
 };
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const oldQueries = {
-//   title: 'Queries',
-//   add: 'Add query',
-//   unsaved: 'Unsaved query',
-//   group: 'Group',
-//   editGroups: 'Edit Groups',
-//   queryGroupSaveFailed: 'Failed to save. Try another name.',
-//   addQueryGroup: 'Add group',
-//   queryGroupName: 'Group name',
-//   selectQueryGroup: 'Select group',
-//   description: 'Description',
-
-//   titleNotUnique: 'This name is already in use.',
-//   duplicate: 'Duplicate',
-
-//   query: 'Query',
-
-//   _BaseTable: 'Base',
-
-//   crossings: 'crossings',
-//   batches: 'batches',
-//   varieties: 'varieties',
-//   plants: 'plants',
-//   motherPlants: 'mother plants',
-//   scionsBundles: 'scions bundles',
-
-//   attributions: 'attributions',
-//   attributions: 'attributions',
-
-//   defaultFilter: 'Filter criteria',
-//   batchFilter: 'Filter criteria to select the batches',
-//   varietyFilter: 'Filter criteria to select the varieties',
-//   plantFilter: 'Filter criteria to select the plants',
-//   markFilter: 'Filter criteria to select the attributions',
-
-//   noFilter:
-//     'No filter criteria defined. All {entity} will be selected. Click the plus button below to add filter criteria.',
-
-//   simplifiable: 'Unnecessary complexity detected.',
-//   simplify: 'Simplify filter',
-//   invalid: 'Invalid filter rules. Rectify or delete them.',
-//   valid: 'Congrats, all rules are valid.',
-
-//   invalidNoResults:
-//     'Invalid filter rules. Rectify or delete them to get results.',
-//   results: 'Results',
-//   addColumn: 'Add Column',
-//   showRowsWithoutAttributions: 'Show rows without attributions',
-
-//   debugShow: 'Show debug info',
-//   debugHide: 'Hide debug info',
-
-//   altPhoto: 'Photo taken {date} by {author}',
-//   photo: 'Photo',
-//   downloadPhoto: 'Download photo',
-
-//   countSuffix: 'count',
-//   maxSuffix: 'max',
-//   minSuffix: 'min',
-//   meanSuffix: 'mean',
-//   medianSuffix: 'median',
-//   stdDevSuffix: 'std. deviation',
-
-//   yes: 'yes',
-//   no: 'no',
-
-//   download: 'Download',
-// };

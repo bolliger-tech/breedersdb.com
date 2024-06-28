fe720553cc48fccba33f6fcdde6a002d7650bc9f
Cyrill Bolliger <mail@cyrill.me>
2024-06-28 23:04:10 +0200

    [BUILD] adapt graphql configs to new url schema

--- graphql.config.js
+++ graphql.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   schema: [
     {
-      'http://localhost:8080/v1/graphql': {
+      'http://localhost/api/v1/graphql': {
         headers: {
           'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
           'x-hasura-role': 'user',

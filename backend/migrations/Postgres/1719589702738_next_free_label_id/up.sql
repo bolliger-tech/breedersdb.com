48942a002e7a98d7c409cafd46e759065dd48801
Cyrill Bolliger <mail@cyrill.me>
2024-06-28 21:00:33 +0200

    [FEAT] next free label id endpoint

---
+++ backend/migrations/Postgres/1719589702738_next_free_label_id/up.sql
@@ -0,0 +1,42 @@
+create or replace function plants_next_free_label_id(input_label_id text) returns setof plants as
+$$
+declare
+    plant_cursor no scroll cursor for select *
+                                      from plants
+                                      where label_id ~ '^[[:digit:]]+$'
+                                        and label_id >= input_label_id
+                                      order by label_id;
+    plant              plants%ROWTYPE;
+    new_plant          plants%ROWTYPE;
+    previous           int;
+    temp_table         plants[];
+    first              boolean := true;
+    input_label_id_int int;
+begin
+    if (input_label_id !~ '^[[:digit:]]+$') then
+        raise exception 'input_label_id must be a string of digits (was: %)', input_label_id;
+    end if;
+
+    input_label_id_int := input_label_id::int;
+    previous := input_label_id_int - 1;
+
+    open plant_cursor;
+    loop
+        fetch plant_cursor into plant;
+        -- if
+        -- -- we reached the end of the table -> return the MAX label_id + 1
+        -- -- or the first plant has a label_id greater than the input_label_id -> return the input_label_id
+        -- -- we found a gap in the label_id sequence -> return the lowest label_id in the gap
+        if not found or (first and plant.label_id::int > input_label_id_int) or
+           (plant.label_id::int - previous > 1) then
+            new_plant.label_id := to_char(previous + 1, 'FM00000000');
+            temp_table := array_append(temp_table, new_plant);
+            return query select * from unnest(array [new_plant]); -- yields to the result set
+            return; -- actually returns
+        end if;
+
+        previous := plant.label_id::int;
+        first := false;
+    end loop;
+end;
+$$ language plpgsql stable;
---
+++ backend/tests/tests/next_free_label_id.test.js
@@ -0,0 +1,235 @@
+import { test, expect, afterEach } from 'bun:test';
+import { post, postOrFail } from '../fetch';
+
+const query = /* GraphQL */ `
+  query NextFreeLabelId($label_id: String!) {
+    plants_next_free_label_id(args: { input_label_id: $label_id }) {
+      label_id
+    }
+  }
+`;
+
+const insertMutation = /* GraphQL */ `
+  mutation InsertPlant(
+    $crossing_name: String!
+    $lot_name_segment: String!
+    $orchard_name: String! = "Orchard 1"
+    $cultivar_name_segment: String!
+    $plant_group_name_segment: String! = "A"
+    $label_id: String!
+    $date_eliminated: date = null
+  ) {
+    insert_crossings_one(
+      object: {
+        name: $crossing_name
+        lots: {
+          data: {
+            name_segment: $lot_name_segment
+            orchard: { data: { name: $orchard_name } }
+            cultivars: {
+              data: {
+                name_segment: $cultivar_name_segment
+                plant_groups: {
+                  data: {
+                    name_segment: $plant_group_name_segment
+                    plants: {
+                      data: {
+                        label_id: $label_id
+                        date_eliminated: $date_eliminated
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    ) {
+      id
+    }
+  }
+`;
+
+async function insertWithLabelId({
+  label_id,
+  crossing_name = 'Abcd',
+  orchard_name = 'Orchard 1',
+  eliminated = false,
+}) {
+  await postOrFail({
+    query: insertMutation,
+    variables: {
+      crossing_name,
+      lot_name_segment: '24A',
+      cultivar_name_segment: '001',
+      plant_group_name_segment: 'Z',
+      label_id,
+      date_eliminated: eliminated ? '2024-03-24' : null,
+      orchard_name,
+    },
+  });
+}
+
+afterEach(async () => {
+  await postOrFail({
+    query: /* GraphQL */ `
+      mutation DeleteAllPlants {
+        delete_plants(where: {}) {
+          affected_rows
+        }
+        delete_plant_rows(where: {}) {
+          affected_rows
+        }
+        delete_rootstocks(where: {}) {
+          affected_rows
+        }
+        delete_graftings(where: {}) {
+          affected_rows
+        }
+        delete_plant_groups(where: {}) {
+          affected_rows
+        }
+        delete_cultivars(where: {}) {
+          affected_rows
+        }
+        delete_lots(where: {}) {
+          affected_rows
+        }
+        delete_crossings(where: {}) {
+          affected_rows
+        }
+        delete_orchards(where: {}) {
+          affected_rows
+        }
+      }
+    `,
+  });
+});
+
+test('throws on not digits only input', async () => {
+  const resp = await post({
+    query,
+    variables: {
+      label_id: '#00000001',
+    },
+  });
+
+  expect(resp.errors[0].extensions.internal.error.message).toMatch(
+    /input_label_id must be a string of digits/,
+  );
+});
+
+test('empty table', async () => {
+  const resp = await postOrFail({
+    query,
+    variables: {
+      label_id: '00000001',
+    },
+  });
+
+  expect(resp.data.plants_next_free_label_id[0].label_id).toBe('00000001');
+});
+
+test('free label_id', async () => {
+  await insertWithLabelId({ label_id: '00000001' });
+
+  const resp = await postOrFail({
+    query,
+    variables: {
+      label_id: '00000009',
+    },
+  });
+
+  expect(resp.data.plants_next_free_label_id[0].label_id).toBe('00000009');
+});
+
+test('free label_id with same label_id eliminated', async () => {
+  await insertWithLabelId({
+    label_id: '#00000001',
+    eliminated: true,
+  });
+
+  const resp = await postOrFail({
+    query,
+    variables: {
+      label_id: '00000001',
+    },
+  });
+
+  expect(resp.data.plants_next_free_label_id[0].label_id).toBe('00000001');
+});
+
+test('next is free', async () => {
+  await insertWithLabelId({ label_id: '00000001' });
+
+  const resp = await postOrFail({
+    query,
+    variables: {
+      label_id: '00000001',
+    },
+  });
+
+  expect(resp.data.plants_next_free_label_id[0].label_id).toBe('00000002');
+});
+
+test('+2 is free', async () => {
+  await insertWithLabelId({
+    label_id: '00000001',
+    crossing_name: 'a',
+    orchard_name: 'Orchard 1',
+  });
+  await insertWithLabelId({
+    label_id: '00000002',
+    crossing_name: 'b',
+    orchard_name: 'Orchard 2',
+  });
+
+  const resp = await postOrFail({
+    query,
+    variables: {
+      label_id: '00000001',
+    },
+  });
+
+  expect(resp.data.plants_next_free_label_id[0].label_id).toBe('00000003');
+});
+
+test('fills the gaps', async () => {
+  await insertWithLabelId({
+    label_id: '00000001',
+    crossing_name: 'a',
+    orchard_name: 'Orchard 1',
+  });
+  await insertWithLabelId({
+    label_id: '00000003',
+    crossing_name: 'b',
+    orchard_name: 'Orchard 2',
+  });
+
+  const resp = await postOrFail({
+    query,
+    variables: {
+      label_id: '00000001',
+    },
+  });
+
+  expect(resp.data.plants_next_free_label_id[0].label_id).toBe('00000002');
+});
+
+test('is not lower than the given label id', async () => {
+  await insertWithLabelId({
+    label_id: '00000010',
+    crossing_name: 'a',
+    orchard_name: 'Orchard 1',
+  });
+
+  const resp = await postOrFail({
+    query,
+    variables: {
+      label_id: '00000010',
+    },
+  });
+
+  expect(resp.data.plants_next_free_label_id[0].label_id).toBe('00000011');
+});

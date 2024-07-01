4bd9639be90d65c3d1a86bdcd358018cb0503492
Cyrill Bolliger <mail@cyrill.me>
2024-07-01 21:32:08 +0200

    [FEAT] gallery improvements

--- frontend/src/components/Entity/View/EntityViewAttributionImage.vue
+++ frontend/src/components/Entity/View/EntityViewAttributionImage.vue
@@ -22,8 +22,19 @@
     @click="open = true"
   />
 
-  <q-dialog v-model="open" full-height full-width>
-    <q-card style="width: calc(100svw - 48px); height: calc(100svh - 48px)">
+  <q-dialog
+    v-model="open"
+    :transition-show="transition"
+    :transition-hide="transition"
+    :transition-duration="transitionDuration"
+    @keydown.left="$emit('previous')"
+    @keydown.right="$emit('next')"
+  >
+    <q-card
+      v-touch-swipe.right="() => $emit('previous')"
+      v-touch-swipe.left="() => $emit('next')"
+      style="max-width: calc(100svw - 48px); max-height: calc(100svh - 48px)"
+    >
       <q-card-section class="row justify-end q-pa-sm">
         <q-btn flat dense round icon="close" @click="open = false" />
       </q-card-section>
@@ -39,7 +50,13 @@
           `"
           spinner-color="primary"
           fit="contain"
-          style="width: calc(100svw - 48px); height: calc(100svh - 202px)"
+          style="
+            max-width: calc(100svw - 80px);
+            width: calc(100svw - 80px);
+            max-height: calc(100svh - 202px);
+            height: calc(100svh - 202px);
+          "
+          :draggable="false"
         >
           <template #error>
             <div class="text-caption q-mx-md absolute-center text-center">
@@ -75,9 +92,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import { useI18n } from 'src/composables/useI18n';
 import { EntityAttributionsViewFragment } from '../entityAttributionsViewFragment';
+import { localizeDate } from 'src/utils/dateUtils';
+import { QDialogProps } from 'quasar';
 
 export interface EntityViewAttributionImageProps {
   attribution: EntityAttributionsViewFragment;
@@ -87,25 +106,50 @@ export interface EntityViewAttributionImageProps {
   lot?: { display_name: string };
   crossing?: { name: string };
   preview?: boolean;
+  transition?: QDialogProps['transitionShow'];
+  transitionDuration?: number;
 }
 
 const props = defineProps<EntityViewAttributionImageProps>();
+defineEmits<{
+  next: [];
+  previous: [];
+}>();
 
-const open = ref(false);
+const open = defineModel<boolean>({ required: false, default: false });
+
 const { t } = useI18n();
 
-const desiredFileName = computed(() => {
-  const org = import.meta.env.VITE_ORG_ABBREVIATION;
-  const entityName =
+const entityName = computed(() => {
+  return (
     props.plant?.label_id ??
     props.plantGroup?.display_name ??
     props.cultivar?.display_name ??
     props.lot?.display_name ??
     props.crossing?.name ??
-    'unknown';
+    'unknown'
+  );
+});
+
+const entityType = computed(() => {
+  return props.plant
+    ? t('plants.title', 1)
+    : props.plantGroup
+      ? t('plantGroups.title', 1)
+      : props.cultivar
+        ? t('cultivars.title', 1)
+        : props.lot
+          ? t('lots.title', 1)
+          : props.crossing
+            ? t('crossings.title', 1)
+            : 'unknown';
+});
+
+const desiredFileName = computed(() => {
+  const org = import.meta.env.VITE_ORG_ABBREVIATION;
 
   return encodeURIComponent(
-    `${org}-${entityName}-${props.attribution.attribute_name}-${props.attribution.date_attributed}-${props.attribution.id}.jpg`,
+    `${org}-${entityName.value}-${props.attribution.attribute_name}-${props.attribution.date_attributed}-${props.attribution.id}.jpg`,
   );
 });
 
@@ -114,6 +158,6 @@ const storedFileName = computed(() => {
 });
 
 const metadata = computed(() => {
-  return `${props.attribution.attribute_name}, ${props.attribution.date_attributed} ${props.attribution.author}`;
+  return `${props.attribution.attribute_name}, ${localizeDate(props.attribution.date_attributed)} ${props.attribution.author}, ${entityType.value} ${entityName.value}`;
 });
 </script>
--- frontend/src/components/Entity/View/EntityViewAttributionImageGallery.vue
+++ frontend/src/components/Entity/View/EntityViewAttributionImageGallery.vue
@@ -4,7 +4,7 @@
     style="height: 216px; max-width: 100%; margin: 8px 16px"
   >
     <div class="row no-wrap q-gutter-sm scroll">
-      <div v-for="image of images" :key="image.id">
+      <div v-for="(image, index) in images" :key="image.id">
         <EntityViewAttributionImage
           :attribution="image"
           :plant="plant"
@@ -13,6 +13,17 @@
           :lot="lot"
           :crossing="crossing"
           preview
+          :model-value="open === image.id"
+          :transition="transition"
+          :transition-duration="TRANSITION_DURATION"
+          @update:model-value="(val: boolean) => (open = val ? image.id : null)"
+          @next="show(images[(index + 1) % images.length]?.id, 'left')"
+          @previous="
+            show(
+              images[(index - 1 + images.length) % images.length]?.id,
+              'right',
+            )
+          "
         />
       </div>
     </div>
@@ -27,7 +38,14 @@
 <script setup lang="ts">
 import { useI18n } from 'src/composables/useI18n';
 import { EntityAttributionsViewFragment } from '../entityAttributionsViewFragment';
-import EntityViewAttributionImage from './EntityViewAttributionImage.vue';
+import EntityViewAttributionImage, {
+  EntityViewAttributionImageProps,
+} from './EntityViewAttributionImage.vue';
+import { ref } from 'vue';
+import { useTimeout } from 'quasar';
+import { onBeforeUnmount } from 'vue';
+
+const TRANSITION_DURATION = 300;
 
 export interface EntityViewAttributionsImageGalleryProps {
   images: EntityAttributionsViewFragment[];
@@ -40,4 +58,22 @@ export interface EntityViewAttributionsImageGalleryProps {
 
 defineProps<EntityViewAttributionsImageGalleryProps>();
 const { t } = useI18n();
+
+const open = ref<number | null>(null);
+const transition =
+  ref<EntityViewAttributionImageProps['transition']>(undefined);
+
+const transitionTimeout = useTimeout();
+
+function show(id: number | null, direction: 'right' | 'left') {
+  transition.value = `slide-${direction}`;
+  open.value = id;
+  transitionTimeout.registerTimeout(() => {
+    transition.value = undefined;
+  }, TRANSITION_DURATION);
+}
+
+onBeforeUnmount(() => {
+  transitionTimeout.removeTimeout();
+});
 </script>

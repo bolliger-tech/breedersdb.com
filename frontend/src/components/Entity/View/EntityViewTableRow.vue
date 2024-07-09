ee6654ba06c107164e6f31a2b26dbc6511ad95d1
patte <p@tte.io>
2024-07-09 18:04:48 +0200

    [FIX] cleanup css in EntityViewTableRow
    Co-authored-by: Cyrill Bolliger <mail@cyrill.me>

--- frontend/src/components/Entity/View/EntityViewTableRow.vue
+++ frontend/src/components/Entity/View/EntityViewTableRow.vue
@@ -23,28 +23,26 @@ $border: 1px solid $grey-4;
 .entity-view-table-row {
   white-space: nowrap;
   border-bottom: $border;
-}
-.entity-view-table-row:hover {
-  background: rgba(0, 0, 0, 0.03);
-}
-.body--dark .entity-view-table-row {
-  border-color: $grey-8;
-}
-.body--dark .entity-view-table-row:hover {
-  background: rgba(255, 255, 255, 0.07);
-}
-.entity-view-table-row:first-child {
-  border-top: $border;
-}
-
-.entity-view-table-row th {
-  text-align: left;
-  padding: 4px 8px;
-  font-weight: bold;
-}
-
-.entity-view-table-row td {
-  text-align: right;
-  padding: 4px 8px;
+  &:first-child {
+    border-top: $border;
+  }
+  .body--dark & {
+    border-color: $grey-8;
+  }
+  &:hover {
+    background: rgba(0, 0, 0, 0.03);
+    .body--dark & {
+      background: rgba(255, 255, 255, 0.07);
+    }
+  }
+  th {
+    text-align: left;
+    padding: 4px 8px;
+    font-weight: bold;
+  }
+  td {
+    text-align: right;
+    padding: 4px 8px;
+  }
 }
 </style>

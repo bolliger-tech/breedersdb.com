cc7b08cb46946e2a531187a5a0e584b668c40625
Cyrill Bolliger <mail@cyrill.me>
2024-07-12 23:42:10 +0200

    [FEAT] explainer max width

--- frontend/src/components/Base/BaseExplainer.vue
+++ frontend/src/components/Base/BaseExplainer.vue
@@ -1,6 +1,6 @@
 <template>
   <q-icon name="help" class="base-explainer">
-    <q-tooltip><slot></slot></q-tooltip>
+    <q-tooltip style="width: min(90svw, 500px)"><slot></slot></q-tooltip>
   </q-icon>
 </template>
 

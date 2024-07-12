f79f151e0b43788a00143ffc525588ac0a5fe469
Cyrill Bolliger <mail@cyrill.me>
2024-07-12 10:41:25 +0200

    [FIX] comment out erroneous code - REVERT LATER!

--- frontend/src/components/Attribute/AttributeForm.vue
+++ frontend/src/components/Attribute/AttributeForm.vue
@@ -5,11 +5,11 @@
         v-for="formField in form.attribution_form_fields"
         :key="formField.priority"
       >
-        <AttributeFormInput
+        <!-- <AttributeFormInput
           v-model="attributeValues[formField.priority]"
           :attribute="formField.attribute"
           :exceptional="false"
-        />
+        /> -->
       </li>
     </ul>
     <!-- TODO: add exceptional attributions -->
@@ -19,12 +19,12 @@
 
 <script setup lang="ts">
 import type { AttributionForm } from 'src/components/Attribute/AttributeSteps.vue';
-import { useI18n } from 'src/composables/useI18n';
+// import { useI18n } from 'src/composables/useI18n';
 import { graphql, VariablesOf } from 'src/graphql';
-import { useMutation } from '@urql/vue';
+// import { useMutation } from '@urql/vue';
 import { AttributableEntities } from 'src/components/Attribute/attributableEntities';
-import { ref } from 'vue';
-import AttributeFormInput from 'src/components/Attribute/AttributeFormInput.vue';
+// import { ref } from 'vue';
+// import AttributeFormInput from 'src/components/Attribute/AttributeFormInput.vue';
 
 export interface AttributeFormProps {
   entityId: number;
@@ -38,13 +38,14 @@ export type AttributeInsertData = VariablesOf<
   typeof mutation
 >['attributeValues'][0];
 
-const props = defineProps<AttributeFormProps>();
+// const props = defineProps<AttributeFormProps>();
+defineProps<AttributeFormProps>();
 
-const { t } = useI18n();
+// const { t } = useI18n();
 
 // !!! uses the PRIORITY as the key !!!
 // (to allow multiple inserts of the same attribute)
-const attributeValues = ref<{ [key: number]: AttributeInsertData }>({});
+// const attributeValues = ref<{ [key: number]: AttributeInsertData }>({});
 
 const mutation = graphql(`
   mutation InsertAttributions(
@@ -75,47 +76,47 @@ const mutation = graphql(`
 `);
 
 // TODO: handle photos
-const {
-  executeMutation: insertAttributions,
-  fetching: saving,
-  error,
-} = useMutation(mutation);
+// const {
+//   executeMutation: insertAttributions,
+//   fetching: saving,
+//   error,
+// } = useMutation(mutation);
 
-async function save() {
-  const values = Object.values(attributeValues.value).filter(
-    (av) =>
-      // do not store attribution_values without a value
-      av.integer_value !== null ||
-      av.float_value !== null ||
-      av.text_value !== null ||
-      av.boolean_value !== null ||
-      av.date_value !== null,
-  );
+// async function save() {
+//   const values = Object.values(attributeValues.value).filter(
+//     (av) =>
+//       // do not store attribution_values without a value
+//       av.integer_value !== null ||
+//       av.float_value !== null ||
+//       av.text_value !== null ||
+//       av.boolean_value !== null ||
+//       av.date_value !== null,
+//   );
 
-  if (values.length === 0) {
-    // do not store attributions without any values
-    return;
-  }
+//   if (values.length === 0) {
+//     // do not store attributions without any values
+//     return;
+//   }
 
-  await insertAttributions({
-    formId: props.form.id,
-    author: props.author,
-    dateAttributed: props.date,
-    lotId:
-      props.entityType === AttributableEntities.Lot ? props.entityId : null,
-    cultivarId:
-      props.entityType === AttributableEntities.Cultivar
-        ? props.entityId
-        : null,
-    plantGroupId:
-      props.entityType === AttributableEntities.PlantGroup
-        ? props.entityId
-        : null,
-    plantId:
-      props.entityType === AttributableEntities.Plant ? props.entityId : null,
-    attributeValues: values,
-  });
-}
+//   await insertAttributions({
+//     formId: props.form.id,
+//     author: props.author,
+//     dateAttributed: props.date,
+//     lotId:
+//       props.entityType === AttributableEntities.Lot ? props.entityId : null,
+//     cultivarId:
+//       props.entityType === AttributableEntities.Cultivar
+//         ? props.entityId
+//         : null,
+//     plantGroupId:
+//       props.entityType === AttributableEntities.PlantGroup
+//         ? props.entityId
+//         : null,
+//     plantId:
+//       props.entityType === AttributableEntities.Plant ? props.entityId : null,
+//     attributeValues: values,
+//   });
+// }
 </script>
 
 <style scoped lang="scss">

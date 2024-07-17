42ab657fd322b7cae6350e1fbb0f56866b66d333
patte <p@tte.io>
2024-07-17 19:14:49 +0200

    [FIX] cleanup not working client side sort

--- frontend/src/pages/Graftings/IndexPage.vue
+++ frontend/src/pages/Graftings/IndexPage.vue
@@ -27,10 +27,8 @@ import { useQueryArg } from 'src/composables/useQueryArg';
 import EntityContainer from 'src/components/Entity/EntityContainer.vue';
 import { graftingFragment } from 'src/components/Grafting/graftingFragment';
 import { useEntityIndexHooks } from 'src/composables/useEntityIndexHooks';
-import { useLocalizedSort } from 'src/composables/useLocalizedSort';
 
 const { t, d } = useI18n();
-const { localizedSortPredicate } = useLocalizedSort();
 
 const query = graphql(
   `
@@ -78,7 +76,6 @@ const columns = [
     align: 'left' as const,
     field: 'name',
     sortable: true,
-    sort: (a: Grafting, b: Grafting) => localizedSortPredicate(a.name, b.name),
   },
   {
     name: 'modified',

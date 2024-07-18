c5a47ca0cc323df4f2c6231791553b204181dc58
Cyrill Bolliger <mail@cyrill.me>
2024-07-18 15:50:10 +0200

    [WIP] analyze index page

---
+++ frontend/src/pages/Cultivars/AnalyzeIndexPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <PageLayout>
+    TODO
+    <!-- TODO: EntityList -->
+  </PageLayout>
+</template>
+
+<script setup lang="ts">
+import PageLayout from 'src/layouts/PageLayout.vue';
+</script>

48651107725ba8cc9f168ec3c142989dc027c151
Cyrill Bolliger <mail@cyrill.me>
2024-07-22 12:00:57 +0200

    [FIX] detection of network error

--- frontend/src/components/Base/BaseGraphqlError.vue
+++ frontend/src/components/Base/BaseGraphqlError.vue
@@ -1,6 +1,6 @@
 <template>
   <article class="q-my-md q-pa-sm bg-black text-negative rounded-borders">
-    <template v-if="error.networkError?.message === 'Failed to fetch'">
+    <template v-if="isOfflineError">
       <BaseMessage type="error" icon-size="xl">
         <h3 class="q-my-none col text-white">
           {{ t('base.networkError.title') }}
@@ -29,7 +29,7 @@
 
 <script lang="ts" setup>
 import { CombinedError } from '@urql/core';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { captureException } from '@sentry/browser';
 import { useI18n } from 'src/composables/useI18n';
 import BaseMessage from 'src/components/Base/BaseMessage.vue';
@@ -42,6 +42,17 @@ export interface BaseGraphqlErrorProps {
 const props = withDefaults(defineProps<BaseGraphqlErrorProps>(), {
   throw: false,
 });
+
+// adapted from https://github.com/urql-graphql/urql/blob/65e15c568c9a0755894dfb02995f0262e1fe75f9/exchanges/graphcache/src/offlineExchange.ts#L59
+const isOfflineError = computed(
+  () =>
+    props.error.networkError &&
+    !props.error.response &&
+    ((typeof navigator !== 'undefined' && navigator.onLine === false) ||
+      /request failed|failed to fetch|network\s?error/i.test(
+        props.error.networkError.message,
+      )),
+);
 
 onMounted(() => {
   if (props.error && props.throw) {

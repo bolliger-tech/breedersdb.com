a16fc523b71f477f3c538b08aaa8fe494fafcbbc
cyrillbolliger <cyrillbolliger@users.noreply.github.com>
2024-07-31 14:49:52 +0000

    Latest translations from Crowdin

---
+++ frontend/src/i18n/de-CH/cultivars.ts
@@ -0,0 +1,11 @@
+export const cultivars = {
+  title: 'Cultivar | Cultivars',
+  analyze: {
+    title: 'Analyze'
+  },
+  fields: {
+    acronym: 'Acronym',
+    breeder: 'Breeder',
+    registration: 'Registration'
+  }
+};
---
+++ frontend/src/i18n/de-CH/lots.ts
@@ -0,0 +1,14 @@
+export const lots = {
+  title: 'Lot | Lots',
+  fields: {
+    dateSowed: 'Date sowed',
+    numbSeedsSowed: 'Number of seeds sowed',
+    numbSeedlingsGrown: 'Number of seedlings grown',
+    seedTray: 'Seed tray',
+    datePlanted: 'Date planted',
+    numbSeedlingsPlanted: 'Number of seedlings planted',
+    plot: 'Plot',
+    crossing: 'Crossing',
+    orchard: 'Orchard'
+  }
+};

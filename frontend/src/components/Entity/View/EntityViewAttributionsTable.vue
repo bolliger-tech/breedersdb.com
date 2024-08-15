aa794d2945493c70dd93c55c26db251641bd3ea8
Cyrill Bolliger <mail@cyrill.me>
2024-08-15 19:00:16 +0200

    [FEAT] show entity in attributions table

--- frontend/src/components/Entity/View/EntityViewAttributionsTable.vue
+++ frontend/src/components/Entity/View/EntityViewAttributionsTable.vue
@@ -6,6 +6,39 @@
     default-sort-by="date_attributed"
     :default-descending="true"
   >
+    <template #body-cell-entity="cellProps">
+      <q-td key="entity" :props="cellProps">
+        <RouterLink
+          v-if="cellProps.row.plant"
+          :to="`/plants/${cellProps.row.plant.id}`"
+          class="undecorated-link"
+        >
+          <PlantLabelId :label-id="cellProps.row.plant.label_id" />
+        </RouterLink>
+        <RouterLink
+          v-else-if="cellProps.row.plant_group"
+          :to="`/groups/${cellProps.row.plant_group.id}`"
+          class="undecorated-link"
+        >
+          {{ cellProps.row.plant_group.display_name }}
+        </RouterLink>
+        <RouterLink
+          v-else-if="cellProps.row.cultivar"
+          :to="`/cultivars/${cellProps.row.cultivar.id}`"
+          class="undecorated-link"
+        >
+          {{ cellProps.row.cultivar.display_name }}
+        </RouterLink>
+        <RouterLink
+          v-else-if="cellProps.row.lot"
+          :to="`/lots/${cellProps.row.lot.id}`"
+          class="undecorated-link"
+        >
+          {{ cellProps.row.lot.name }}
+        </RouterLink>
+      </q-td>
+    </template>
+
     <template #body-cell-value="cellProps">
       <q-td key="value" :props="cellProps">
         <template v-if="cellProps.row.data_type === 'PHOTO'">
@@ -56,10 +89,13 @@ import { localizeDate } from 'src/utils/dateUtils';
 import EntityViewAttributionImage from './EntityViewAttributionImage.vue';
 import { ColumnTypes } from 'src/utils/columnTypes';
 import EntityViewRelatedEntityTable from './EntityViewRelatedEntityTable.vue';
+import PlantLabelId from 'src/components/Plant/PlantLabelId.vue';
+import { useLocalizedSort } from 'src/composables/useLocalizedSort';
 
 export interface EntityViewAttributionsTableProps {
   rows: EntityAttributionsViewFragment[];
   attributeType?: AttributeTypes;
+  showEntity?: boolean;
   plant?: { label_id: string };
   plantGroup?: { display_name: string };
   cultivar?: { display_name: string };
@@ -70,8 +106,93 @@ export interface EntityViewAttributionsTableProps {
 const props = defineProps<EntityViewAttributionsTableProps>();
 
 const { t } = useI18n();
+const { localizedSortPredicate } = useLocalizedSort();
 
 const columns = [
+  ...(props.showEntity
+    ? [
+        {
+          name: 'entity',
+          label: t('attributions.columns.entity'),
+          field: (row: EntityAttributionsViewFragment) => row,
+          align: 'left' as const,
+          sortable: true,
+          sort: (
+            a: EntityAttributionsViewFragment,
+            b: EntityAttributionsViewFragment,
+          ) => {
+            // sort grouped by plant, plant_group, cultivar, lot
+            if (a.plant && b.plant) {
+              return localizedSortPredicate(a.plant.label_id, b.plant.label_id);
+            }
+
+            if (!a.plant && !b.plant && a.plant_group && b.plant_group) {
+              return localizedSortPredicate(
+                a.plant_group.display_name,
+                b.plant_group.display_name,
+              );
+            }
+
+            if (
+              !a.plant &&
+              !b.plant &&
+              !a.plant_group &&
+              !b.plant_group &&
+              a.cultivar &&
+              b.cultivar
+            ) {
+              return localizedSortPredicate(
+                a.cultivar.display_name,
+                b.cultivar.display_name,
+              );
+            }
+
+            if (
+              !a.plant &&
+              !b.plant &&
+              !a.plant_group &&
+              !b.plant_group &&
+              !a.cultivar &&
+              !b.cultivar &&
+              a.lot &&
+              b.lot
+            ) {
+              return localizedSortPredicate(
+                a.lot.display_name,
+                b.lot.display_name,
+              );
+            }
+
+            if (a.plant && !b.plant) {
+              return -1;
+            }
+            if (!a.plant && b.plant) {
+              return 1;
+            }
+            if (a.plant_group && !b.plant_group) {
+              return -1;
+            }
+            if (!a.plant_group && b.plant_group) {
+              return 1;
+            }
+            if (a.cultivar && !b.cultivar) {
+              return -1;
+            }
+            if (!a.cultivar && b.cultivar) {
+              return 1;
+            }
+            if (a.lot && !b.lot) {
+              return -1;
+            }
+            if (!a.lot && b.lot) {
+              return 1;
+            }
+
+            return 0;
+          },
+        },
+      ]
+    : []),
   {
     name: 'attribute_name',
     label: t('attributions.columns.attributeName'),

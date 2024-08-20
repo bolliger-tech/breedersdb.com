1d4329c5f9876d9dc8839786e1c111fda4c258a4
cyrillbolliger <12530490+cyrillbolliger@users.noreply.github.com>
2024-08-20 09:17:26 +0000

    Latest translations from Crowdin

---
+++ frontend/src/i18n/de-CH/cultivars.ts
@@ -0,0 +1,16 @@
+export const cultivars = {
+  title: 'Cultivar | Cultivars',
+  searchPlaceholder: 'Search by name or acronym',
+  analyze: {
+    title: 'Analyze'
+  },
+  fields: {
+    acronym: 'Acronym',
+    breeder: 'Breeder',
+    registration: 'Registration',
+    lot: 'Lot'
+  },
+  validation: {
+    nameNotUniqueWithLot: 'This name is already in use in combination with the selected lot'
+  }
+};
---
+++ frontend/src/i18n/de-CH/pollen.ts
@@ -0,0 +1,8 @@
+export const pollen = {
+  title: 'Pollen | Pollen',
+  searchPlaceholder: 'Search by name or cultivar',
+  fields: {
+    cultivarName: 'Cultivar',
+    dateHarvested: 'Date harvested'
+  }
+};

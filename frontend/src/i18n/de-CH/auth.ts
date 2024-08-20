156497bd8e94464ac49280d3a81bdb47ff50efef
cyrillbolliger <12530490+cyrillbolliger@users.noreply.github.com>
2024-08-20 14:09:30 +0000

    Latest translations from Crowdin

---
+++ frontend/src/i18n/de-CH/auth.ts
@@ -0,0 +1,15 @@
+export const auth = {
+  email: 'Email',
+  password: 'Password',
+  signInTitle: 'Sign In',
+  signInFooter: 'breedersdb.com',
+  signInButton: 'Sign In',
+  signOutMessage: 'Are you sure you want to sign out?',
+  signOutButton: 'Sign Out',
+  errors: {
+    401: 'This password is not correct. {nextTry}',
+    429: 'Too many failed sign in attempts. {nextTry}',
+    404: 'This user was not found.',
+    nextTry: 'Next possible try {fromNow}'
+  }
+};
---
+++ frontend/src/i18n/de-CH/nav.ts
@@ -0,0 +1,37 @@
+export const nav = {
+  shared: {
+    list: 'List',
+    new: 'New',
+    addAttribute: 'Add attribute',
+    analyze: 'Analyze'
+  },
+  plants: {
+    plant: 'Plant',
+    eliminate: 'Eliminate'
+  },
+  cultivars: {
+    new: {
+      main: 'New',
+      breedersCultivar: 'Breeders Cultivar',
+      variety: 'Variety'
+    }
+  },
+  more: {
+    main: 'More',
+    attributionForms: {
+      main: 'Attribution Forms',
+      forms: 'Forms',
+      attributes: 'Attributes'
+    },
+    settings: 'Settings',
+    darkMode: {
+      main: 'Dark mode',
+      on: 'On',
+      off: 'Off'
+    },
+    language: {
+      main: 'Language'
+    },
+    logout: 'Logout'
+  }
+};

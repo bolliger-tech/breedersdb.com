de8cb2691d3a3832bc4bca76a4e16c36ac5ee645
Cyrill Bolliger <mail@cyrill.me>
2024-08-24 19:37:27 +0200

    [FEAT] attribute cultivars

---
+++ frontend/src/components/Cultivar/CultivarSelector.vue
@@ -0,0 +1,173 @@
+<template>
+  <EntitySelector
+    ref="inputRef"
+    entity-type="cultivar"
+    :error="error?.message"
+    @input="
+      {
+        input = $event;
+        loadCultivar();
+      }
+    "
+  />
+  <BaseGraphqlError v-if="fetchError" :error="fetchError" />
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'src/composables/useI18n';
+import { ref, watch, nextTick, onBeforeUnmount } from 'vue';
+import BaseGraphqlError from '../Base/BaseGraphqlError.vue';
+import { computed } from 'vue';
+import { graphql } from 'src/graphql';
+import { useQuery } from '@urql/vue';
+import { CultivarFragment, cultivarFragment } from './cultivarFragment';
+import { onMounted } from 'vue';
+import EntitySelector from 'src/components/Entity/EntitySelector.vue';
+
+const emit = defineEmits<{
+  cultivar: [data: CultivarFragment | null];
+  fetching: [data: boolean];
+}>();
+onMounted(() => emit('cultivar', null));
+
+const inputRef = ref<InstanceType<typeof EntitySelector> | null>(null);
+
+defineExpose({
+  loadEntity: async () => {
+    inputRef.value?.emitInputs();
+    await nextTick();
+    loadCultivar();
+  },
+  focus: () => {
+    inputRef.value?.focus();
+  },
+});
+
+const input = ref<{
+  plantLabelId: string;
+  plantGroupLabelId: string;
+  cultivarId: number | null;
+  lotId: number | null;
+}>({
+  plantLabelId: '',
+  plantGroupLabelId: '',
+  cultivarId: null,
+  lotId: null,
+});
+
+const { t } = useI18n();
+
+const error = computed<
+  | {
+      type: 'notFound';
+      message: string;
+    }
+  | undefined
+>(() => {
+  if (
+    data.value?.cultivars.length === 0 &&
+    (input.value.plantLabelId || input.value.plantGroupLabelId)
+  )
+    return {
+      type: 'notFound',
+      message: input.value.plantLabelId
+        ? t('plants.errors.labelIdNotFound', {
+            labelId: input.value.plantLabelId,
+          })
+        : t('plantGroups.errors.labelIdNotFound', {
+            labelId: input.value.plantGroupLabelId,
+          }),
+    };
+  return undefined;
+});
+
+const queryCultivarByPlantLabelId = graphql(
+  `
+    query CultivarByPlantLabelId(
+      $labelId: String!
+      $CultivarWithLot: Boolean! = true
+      $LotWithCrossing: Boolean! = true
+      $LotWithOrchard: Boolean! = false
+    ) {
+      cultivars(
+        where: { plant_groups: { plants: { label_id: { _eq: $labelId } } } }
+      ) {
+        ...cultivarFragment
+      }
+    }
+  `,
+  [cultivarFragment],
+);
+const queryCultivarByPlantGroupLabelId = graphql(
+  `
+    query CultivarByPlantGroupLabelId(
+      $labelId: String!
+      $CultivarWithLot: Boolean! = true
+      $LotWithCrossing: Boolean! = true
+      $LotWithOrchard: Boolean! = false
+    ) {
+      cultivars(where: { plant_groups: { label_id: { _eq: $labelId } } }) {
+        ...cultivarFragment
+      }
+    }
+  `,
+  [cultivarFragment],
+);
+const queryCultivarById = graphql(
+  `
+    query CultivarById(
+      $id: Int!
+      $CultivarWithLot: Boolean! = true
+      $LotWithCrossing: Boolean! = true
+      $LotWithOrchard: Boolean! = false
+    ) {
+      cultivars(where: { id: { _eq: $id } }) {
+        ...cultivarFragment
+      }
+    }
+  `,
+  [cultivarFragment],
+);
+const query = computed(() =>
+  input.value.cultivarId
+    ? queryCultivarById
+    : input.value.plantLabelId
+      ? queryCultivarByPlantLabelId
+      : queryCultivarByPlantGroupLabelId,
+);
+
+const labelIdForQuery = computed(() => ({
+  labelId: input.value.plantLabelId || input.value.plantGroupLabelId || '',
+}));
+const cultivarIdForQuery = computed(() => ({
+  id: input.value.cultivarId || 0,
+}));
+const variables = computed(() =>
+  input.value.cultivarId ? cultivarIdForQuery.value : labelIdForQuery.value,
+);
+
+const {
+  executeQuery,
+  fetching,
+  data,
+  error: fetchError,
+} = await useQuery({
+  query,
+  variables,
+  pause: true,
+});
+
+watch(fetching, (f) => emit('fetching', f));
+onBeforeUnmount(() => emit('fetching', false));
+
+watch(data, (d) => {
+  if (d?.cultivars.length) {
+    emit('cultivar', d.cultivars[0]);
+  }
+});
+
+async function loadCultivar() {
+  await nextTick(); // ensure the useQuery({variables}) is updated
+  await executeQuery();
+}
+</script>
---
+++ frontend/src/pages/Cultivars/AddAttributionPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <PageLayout>
+    <h1>
+      {{
+        t('attributions.add.attributeEntity', {
+          entity: t('cultivars.title', 1),
+        })
+      }}
+    </h1>
+    <AttributionAddSteps
+      :entity-caption="entityCaption"
+      :entity-loading="fetching"
+      :entity-id="cultivar?.id || null"
+      :entity-type="AttributableEntities.Cultivar"
+      :focus-entity-selector="cultivarSelectorRef?.focus"
+      entity-icon="svguse:/icons/sprite.svg#cultivar"
+      @entity-step-completed="() => cultivarSelectorRef?.loadEntity()"
+    >
+      <template #entity-selector>
+        <CultivarSelector
+          ref="cultivarSelectorRef"
+          @cultivar="(c) => (cultivar = c)"
+          @fetching="(f) => (fetching = f)"
+        />
+      </template>
+
+      <template #entity-preview>
+        <EntityCard
+          v-if="cultivar"
+          :cultivar="cultivar"
+          entity-type="cultivar"
+        />
+        <!-- if the cultivar is missing AttributionAddSteps will handle it -->
+      </template>
+    </AttributionAddSteps>
+  </PageLayout>
+</template>
+
+<script setup lang="ts">
+import PageLayout from 'src/layouts/PageLayout.vue';
+import { useI18n } from 'src/composables/useI18n';
+import AttributionAddSteps from 'src/components/Attribution/Add/AttributionAddSteps.vue';
+import CultivarSelector from 'src/components/Cultivar/CultivarSelector.vue';
+import EntityCard from 'src/components/Entity/EntityCard.vue';
+import { computed, ref } from 'vue';
+import { CultivarFragment } from 'src/components/Cultivar/cultivarFragment';
+import { AttributableEntities } from 'src/components/Attribution/attributableEntities';
+
+const { t } = useI18n();
+
+const cultivarSelectorRef = ref<InstanceType<typeof CultivarSelector> | null>(
+  null,
+);
+
+const cultivar = ref<CultivarFragment | null>(null);
+const fetching = ref(false);
+
+const entityCaption = computed(() => {
+  if (!cultivar.value) {
+    return undefined;
+  }
+  return cultivar.value.display_name;
+});
+</script>

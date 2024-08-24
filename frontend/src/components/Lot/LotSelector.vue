fc0dca87b7636206eac25f7095f820ea87b0721e
Cyrill Bolliger <mail@cyrill.me>
2024-08-24 20:20:55 +0200

    [FEAT] attribute lots

---
+++ frontend/src/components/Lot/LotSelector.vue
@@ -0,0 +1,194 @@
+<template>
+  <EntitySelector
+    ref="inputRef"
+    entity-type="lot"
+    :error="error?.message"
+    @input="
+      {
+        input = $event;
+        loadLot();
+      }
+    "
+  />
+  <BaseGraphqlError v-if="fetchError" :error="fetchError" />
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'src/composables/useI18n';
+import { ref, watch, nextTick, onBeforeUnmount } from 'vue';
+import BaseGraphqlError from '../Base/BaseGraphqlError.vue';
+import { computed } from 'vue';
+import { graphql } from 'src/graphql';
+import { useQuery } from '@urql/vue';
+import { LotFragment, lotFragment } from './lotFragment';
+import { onMounted } from 'vue';
+import EntitySelector from 'src/components/Entity/EntitySelector.vue';
+
+const emit = defineEmits<{
+  lot: [data: LotFragment | null];
+  fetching: [data: boolean];
+}>();
+onMounted(() => emit('lot', null));
+
+const inputRef = ref<InstanceType<typeof EntitySelector> | null>(null);
+
+defineExpose({
+  loadEntity: async () => {
+    inputRef.value?.emitInputs();
+    await nextTick();
+    loadLot();
+  },
+  focus: () => {
+    inputRef.value?.focus();
+  },
+});
+
+const input = ref<{
+  plantLabelId: string;
+  plantGroupLabelId: string;
+  cultivarId: number | null;
+  lotId: number | null;
+}>({
+  plantLabelId: '',
+  plantGroupLabelId: '',
+  cultivarId: null,
+  lotId: null,
+});
+
+const { t } = useI18n();
+
+const error = computed<
+  | {
+      type: 'notFound';
+      message: string;
+    }
+  | undefined
+>(() => {
+  if (
+    data.value?.lots.length === 0 &&
+    (input.value.plantLabelId || input.value.plantGroupLabelId)
+  )
+    return {
+      type: 'notFound',
+      message: input.value.plantLabelId
+        ? t('plants.errors.labelIdNotFound', {
+            labelId: input.value.plantLabelId,
+          })
+        : t('plantGroups.errors.labelIdNotFound', {
+            labelId: input.value.plantGroupLabelId,
+          }),
+    };
+  return undefined;
+});
+
+const queryLotByPlantLabelId = graphql(
+  `
+    query LotByPlantLabelId(
+      $labelId: String!
+      $LotWithCrossing: Boolean! = true
+      $LotWithOrchard: Boolean! = false
+    ) {
+      lots(
+        where: {
+          cultivars: {
+            plant_groups: { plants: { label_id: { _eq: $labelId } } }
+          }
+        }
+      ) {
+        ...lotFragment
+      }
+    }
+  `,
+  [lotFragment],
+);
+const queryLotByPlantGroupLabelId = graphql(
+  `
+    query LotByPlantGroupLabelId(
+      $labelId: String!
+      $LotWithCrossing: Boolean! = true
+      $LotWithOrchard: Boolean! = false
+    ) {
+      lots(
+        where: { cultivars: { plant_groups: { label_id: { _eq: $labelId } } } }
+      ) {
+        ...lotFragment
+      }
+    }
+  `,
+  [lotFragment],
+);
+const queryLotByCultivarId = graphql(
+  `
+    query LotByCultivarId(
+      $id: Int!
+      $LotWithCrossing: Boolean! = true
+      $LotWithOrchard: Boolean! = false
+    ) {
+      lots(where: { cultivars: { id: { _eq: $id } } }) {
+        ...lotFragment
+      }
+    }
+  `,
+  [lotFragment],
+);
+const queryLotById = graphql(
+  `
+    query LotById(
+      $id: Int!
+      $LotWithCrossing: Boolean! = true
+      $LotWithOrchard: Boolean! = false
+    ) {
+      lots(where: { id: { _eq: $id } }) {
+        ...lotFragment
+      }
+    }
+  `,
+  [lotFragment],
+);
+const query = computed(() =>
+  input.value.lotId
+    ? queryLotById
+    : input.value.cultivarId
+      ? queryLotByCultivarId
+      : input.value.plantLabelId
+        ? queryLotByPlantLabelId
+        : queryLotByPlantGroupLabelId,
+);
+
+const labelIdForQuery = computed(() => ({
+  labelId: input.value.plantLabelId || input.value.plantGroupLabelId || '',
+}));
+const entityIdForQuery = computed(() => ({
+  id: input.value.lotId || input.value.cultivarId || 0,
+}));
+const variables = computed(() =>
+  input.value.lotId || input.value.cultivarId
+    ? entityIdForQuery.value
+    : labelIdForQuery.value,
+);
+
+const {
+  executeQuery,
+  fetching,
+  data,
+  error: fetchError,
+} = await useQuery({
+  query,
+  variables,
+  pause: true,
+});
+
+watch(fetching, (f) => emit('fetching', f));
+onBeforeUnmount(() => emit('fetching', false));
+
+watch(data, (d) => {
+  if (d?.lots.length) {
+    emit('lot', d.lots[0]);
+  }
+});
+
+async function loadLot() {
+  await nextTick(); // ensure the useQuery({variables}) is updated
+  await executeQuery();
+}
+</script>
---
+++ frontend/src/pages/Lots/AddAttributionPage.vue
@@ -0,0 +1,58 @@
+<template>
+  <PageLayout>
+    <h1>
+      {{
+        t('attributions.add.attributeEntity', {
+          entity: t('lots.title', 1),
+        })
+      }}
+    </h1>
+    <AttributionAddSteps
+      :entity-caption="entityCaption"
+      :entity-loading="fetching"
+      :entity-id="lot?.id || null"
+      :entity-type="AttributableEntities.Lot"
+      :focus-entity-selector="lotSelectorRef?.focus"
+      entity-icon="svguse:/icons/sprite.svg#lot"
+      @entity-step-completed="() => lotSelectorRef?.loadEntity()"
+    >
+      <template #entity-selector>
+        <LotSelector
+          ref="lotSelectorRef"
+          @lot="(l) => (lot = l)"
+          @fetching="(f) => (fetching = f)"
+        />
+      </template>
+
+      <template #entity-preview>
+        <EntityCard v-if="lot" entity-type="lot" :lot="lot" />
+        <!-- if the lot is missing AttributionAddSteps will handle it -->
+      </template>
+    </AttributionAddSteps>
+  </PageLayout>
+</template>
+
+<script setup lang="ts">
+import PageLayout from 'src/layouts/PageLayout.vue';
+import { useI18n } from 'src/composables/useI18n';
+import AttributionAddSteps from 'src/components/Attribution/Add/AttributionAddSteps.vue';
+import LotSelector from 'src/components/Lot/LotSelector.vue';
+import EntityCard from 'src/components/Entity/EntityCard.vue';
+import { computed, ref } from 'vue';
+import { LotFragment } from 'src/components/Lot/lotFragment';
+import { AttributableEntities } from 'src/components/Attribution/attributableEntities';
+
+const { t } = useI18n();
+
+const lotSelectorRef = ref<InstanceType<typeof LotSelector> | null>(null);
+
+const lot = ref<LotFragment | null>(null);
+const fetching = ref(false);
+
+const entityCaption = computed(() => {
+  if (!lot.value) {
+    return undefined;
+  }
+  return lot.value.display_name;
+});
+</script>

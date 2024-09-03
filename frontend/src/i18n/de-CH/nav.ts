5eccfbfb4fe3f2555e2c545c2395520c9006c55a
cyrillbolliger <12530490+cyrillbolliger@users.noreply.github.com>
2024-09-03 12:43:11 +0000

    Latest translations from Crowdin

---
+++ frontend/src/i18n/de-CH/nav.ts
@@ -0,0 +1,30 @@
+export const nav = {
+  shared: {
+    list: 'List',
+    new: 'New',
+    addAttribute: 'Add attribute',
+    analyze: 'Analyze'
+  },
+  plants: {
+    plant: 'Plant',
+    eliminate: 'Eliminate'
+  },
+  more: {
+    main: 'More',
+    attributionForms: {
+      main: 'Attribution Forms',
+      forms: 'Forms',
+      attributes: 'Attributes'
+    },
+    settings: 'Settings',
+    darkMode: {
+      main: 'Dark mode',
+      on: 'On',
+      off: 'Off'
+    },
+    language: {
+      main: 'Language'
+    },
+    logout: 'Logout'
+  }
+};

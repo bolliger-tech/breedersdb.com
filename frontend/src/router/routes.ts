07e08bf33884459901dbb529ec4611547817e56c
Cyrill Bolliger <mail@cyrill.me>
2024-09-03 15:08:11 +0200

    [FEAT] add settings WIP

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -149,6 +149,11 @@ const routes: RouteRecordRaw[] = [
       },
 
       {
+        path: 'settings',
+        component: () => import('pages/Settings/IndexPage.vue'),
+      },
+
+      {
         path: 'dev',
         children: [
           { path: '', component: () => import('pages/Dev/IndexPage.vue') },

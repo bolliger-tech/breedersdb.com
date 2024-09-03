a79d9ce65db20167ff4c83cb1da58730cddf2314
Cyrill Bolliger <mail@cyrill.me>
2024-09-03 13:03:31 +0200

    [FIX] type error

--- frontend/src/components/Attribution/attributionValueHasValue.ts
+++ frontend/src/components/Attribution/attributionValueHasValue.ts
@@ -1,8 +1,11 @@
-import type { AttributionValueWithPhoto } from './Add/AttributionAddForm.vue';
-
-export function attributionValueHasValue(
-  attributionValue: Omit<AttributionValueWithPhoto, 'photo_note'>,
-) {
+export function attributionValueHasValue(attributionValue: {
+  integer_value?: number | null | undefined;
+  float_value?: number | null | undefined;
+  text_value?: string | null | undefined;
+  boolean_value?: boolean | null | undefined;
+  date_value?: string | null | undefined;
+  photo_value?: File | null | undefined;
+}) {
   return (
     attributionValue.integer_value !== null ||
     attributionValue.float_value !== null ||

5d650f48f3af32713303d6012e8080b2b6b54192
Cyrill Bolliger <mail@cyrill.me>
2024-09-05 13:59:33 +0200

    [FIX] inconsistent disabling behavoir (closes #133)

--- frontend/src/i18n/en-US/orchards.ts
+++ frontend/src/i18n/en-US/orchards.ts
@@ -3,4 +3,8 @@ export const orchards = {
 
   disableExplainer:
     'Disabling will make it impossible to add rows or lots to this orchard.',
+  disableConfirmation:
+    'Disabling will make it impossible to add rows or lots to this orchard. Do you really want to disable it?',
+  deleteConfirmation:
+    "This orchard doesn't have any rows or lots and will therefore be permanently deleted. Are you sure?",
 };

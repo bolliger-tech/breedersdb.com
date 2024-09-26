025b5c4c6939c8c36477a027205cc3fe677f2041
patte <3500621+patte@users.noreply.github.com>
2024-09-26 20:03:09 +0000

    Latest translations from Crowdin

---
+++ frontend/src/i18n/de-CH/crossings.ts
@@ -0,0 +1,10 @@
+export const crossings = {
+  title: 'Crossing | Crossings',
+  fields: {
+    motherCultivar: 'Mother Cultivar',
+    fatherCultivar: 'Father Cultivar'
+  },
+  hints: {
+    readonlyCultivar: "Mother plants rely on this crossing, therefore the value can't be changed."
+  }
+};
---
+++ frontend/src/i18n/de-CH/plant-rows.ts
@@ -0,0 +1,14 @@
+export const plantRows = {
+  title: 'Row | Rows',
+  fields: {
+    orchard: 'Orchard',
+    dateCreated: 'Date created',
+    dateEliminated: 'Date disabled'
+  },
+  hints: {
+    dateEliminatedTrue: 'Adding an disabled date will disable this row',
+    dateEliminatedFalse: 'Removing the disabled date will enable this row'
+  },
+  disableConfirmation: 'Disabling will make it impossible to add plants to this row. Do you really want to disable it?',
+  deleteConfirmation: "This row doesn't have any plants and will therefore be permanently deleted. Are you sure?"
+};

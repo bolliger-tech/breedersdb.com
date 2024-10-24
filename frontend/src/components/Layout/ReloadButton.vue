d47fce8ca77f5e41e5da64fa6e01c91a3bbb95d1
Cyrill Bolliger <mail@cyrill.me>
2024-10-24 16:11:41 +0200

    [FEAT] force update service workers

--- frontend/src/components/Layout/ReloadButton.vue
+++ frontend/src/components/Layout/ReloadButton.vue
@@ -1,20 +1,32 @@
 <template>
-  <q-item clickable class="column justify-center" @click="reload">
+  <q-item clickable class="column justify-center" @click.stop="reload">
     <figure class="row items-center q-mx-none child-item no-wrap text-white">
-      <q-icon name="refresh" size="sm" />
+      <q-icon v-if="!loading" name="refresh" size="sm" />
+      <q-spinner v-if="loading" size="sm" color="white" />
       <figcaption class="q-ml-md">{{ label }}</figcaption>
     </figure>
   </q-item>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 export interface ReloadButtonProps {
   label: string;
 }
 
 defineProps<ReloadButtonProps>();
 
-function reload() {
-  location.reload();
+const loading = ref(false);
+
+async function reload() {
+  loading.value = true;
+  if ('serviceWorker' in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    const updated = registrations.map((r) => r.update());
+    Promise.allSettled(updated).then(() => location.reload());
+  } else {
+    location.reload();
+  }
 }
 </script>

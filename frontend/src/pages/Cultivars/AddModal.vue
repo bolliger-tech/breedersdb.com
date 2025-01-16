9bd1fff7cd3137b485af04984b4b903a5a5c4169
Cyrill Bolliger <mail@cyrill.me>
2025-01-16 16:24:00 +0100

    [FEAT] create cultivars from template

--- frontend/src/pages/Cultivars/AddModal.vue
+++ frontend/src/pages/Cultivars/AddModal.vue
@@ -1,9 +1,12 @@
 <template>
-  <CultivarModalEdit
-    :cultivar="cultivar"
-    :title="t('base.new')"
-    :is-variety="false"
-  />
+  <EntityFetchWrapper :error="error" :fetching="fetching">
+    <CultivarModalEdit
+      v-if="cultivar"
+      :cultivar="cultivar"
+      :title="t('base.new')"
+      :is-variety="false"
+    />
+  </EntityFetchWrapper>
 </template>
 
 <script setup lang="ts">
@@ -11,8 +14,15 @@ import { useI18n } from 'src/composables/useI18n';
 import CultivarModalEdit, {
   CultivarInsertInput,
 } from 'src/components/Cultivar/CultivarModalEdit.vue';
+import EntityFetchWrapper from 'src/components/Entity/EntityFetchWrapper.vue';
+import { useQuery } from '@urql/vue';
+import { cultivarFragment } from 'src/components/Cultivar/cultivarFragment';
+import { graphql } from 'src/graphql';
+import { computed } from 'vue';
 
-const cultivar: CultivarInsertInput = {
+const props = defineProps<{ templateId?: number; entityId: 'new' }>();
+
+const emptyCultivar: CultivarInsertInput = {
   name_segment: '',
   name_override: null,
   full_name: '',
@@ -21,6 +31,43 @@ const cultivar: CultivarInsertInput = {
   breeder: null,
   note: null,
 };
+
+const query = graphql(
+  `
+    query Cultivar(
+      $id: Int!
+      $CultivarWithLot: Boolean = true
+      $LotWithOrchard: Boolean = false
+      $LotWithCrossing: Boolean = false
+    ) {
+      cultivars_by_pk(id: $id) {
+        ...cultivarFragment
+      }
+    }
+  `,
+  [cultivarFragment],
+);
+
+const { data, error, fetching } = useQuery({
+  query,
+  variables: { id: props.templateId },
+  context: { additionalTypenames: ['cultivars'] },
+  pause: !props.templateId,
+});
+
+const cultivar = computed(() => {
+  if (props.templateId) {
+    if (!data.value?.cultivars_by_pk) {
+      return;
+    }
+    return {
+      ...emptyCultivar,
+      lot_id: data.value.cultivars_by_pk.lot_id,
+    };
+  } else {
+    return emptyCultivar;
+  }
+});
 
 const { t } = useI18n();
 </script>

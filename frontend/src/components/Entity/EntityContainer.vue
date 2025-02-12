c615be1dae25dacea9cf2bc3ac337b19710f191e
Cyrill Bolliger <mail@cyrill.me>
2025-02-12 21:40:29 +0100

    [FEAT] scroll through single entities using the arrow keys

--- frontend/src/components/Entity/EntityContainer.vue
+++ frontend/src/components/Entity/EntityContainer.vue
@@ -38,8 +38,14 @@
     </template>
   </EntityList>
   <EntityModal
-    :model-value="!!$route.params.entityId || !!$route.path.match(/\/new\/?$/)"
+    ref="modal"
+    :key="modalKey"
+    :model-value="entityId !== null || !!$route.path.match(/\/new\/?$/)"
+    :transition-show="transition"
+    :transition-hide="transition"
     @update:model-value="closeModal"
+    @keydown.left.stop.prevent="viewPrevious"
+    @keydown.right.stop.prevent="viewNext"
   >
     <BaseSuspense>
       <template #default>
@@ -63,10 +69,10 @@ import EntityList, { EntityListProps } from './List/EntityList.vue';
 import EntityListTable, {
   EntityListTableProps,
 } from './List/EntityListTable.vue';
-import EntityModal from './EntityModal.vue';
-import { MatcherLocationAsPath, useRoute, useRouter } from 'vue-router';
-import { nextTick, type Slot, computed } from 'vue';
-import { QTableSlots } from 'quasar';
+import EntityModal, { type EntityModalProps } from './EntityModal.vue';
+import { type MatcherLocationAsPath, useRoute, useRouter } from 'vue-router';
+import { nextTick, type Slot, computed, ref, watch } from 'vue';
+import type { QTableSlots } from 'quasar';
 import { useI18n } from 'src/composables/useI18n';
 import BaseSuspense from 'components/Base/BaseSuspense/BaseSuspense.vue';
 import BaseSpinner from 'components/Base/BaseSpinner.vue';
@@ -155,6 +161,46 @@ function closeModal() {
   router.push({ path, query: route.query }).then(() => {
     nextTick(() => nextTick(() => restoreScrollPos()));
   });
+}
+
+const entityId = computed(() => {
+  if (!route.params.entityId) return null;
+  return parseInt(route.params.entityId.toString());
+});
+
+const modalKey = computed(() => entityId.value ?? 'new');
+const modal = ref<InstanceType<typeof EntityModal> | null>(null);
+
+const navigationEnabled = computed(
+  () => entityId.value && props.rows && !modal.value?.persistent,
+);
+
+const transition = ref<EntityModalProps['transition']>(undefined);
+watch(entityId, () => {
+  // use the default transition for opening the modal
+  if (null === entityId.value) transition.value = undefined;
+});
+
+async function viewNext() {
+  if (!navigationEnabled.value) return;
+  const index = props.rows.findIndex((r) => r.id === entityId.value);
+  if (index === -1 || index === props.rows.length - 1) {
+    modal.value?.shake();
+  } else {
+    transition.value = 'slide-left';
+    view(props.rows[index + 1].id);
+  }
+}
+
+async function viewPrevious() {
+  if (!navigationEnabled.value) return;
+  const index = props.rows.findIndex((r) => r.id === entityId.value);
+  if (index === -1 || index === 0) {
+    modal.value?.shake();
+  } else {
+    transition.value = 'slide-right';
+    view(props.rows[index - 1].id);
+  }
 }
 
 const { t } = useI18n();

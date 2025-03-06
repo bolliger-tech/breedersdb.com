659acf711aa66c3b9ff3c6546350f0a399ee7dfd
Cyrill Bolliger <mail@cyrill.me>
2025-03-06 15:44:46 +0100

    [FIX] update nestObject to use non-null assertion for first key

--- frontend/src/composables/useEntityIndexHooks.ts
+++ frontend/src/composables/useEntityIndexHooks.ts
@@ -11,12 +11,12 @@ function nestObject(object: StringObj): StringObj {
     const [first, ...rest] = key.split('.');
 
     if (rest.length === 0) {
-      return { ...acc, [first]: value };
+      return { ...acc, [first!]: value };
     }
 
     return {
       ...acc,
-      [first]: nestObject({ [rest.join('.')]: value }),
+      [first!]: nestObject({ [rest.join('.')]: value }),
     };
   }, {});
 }

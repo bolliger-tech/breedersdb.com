e75d215494b6ae4d0a2d1b83c4407e7cb8a47464
Cyrill Bolliger <mail@cyrill.me>
2025-04-07 10:14:28 +0200

    [REFACTOR] introduce PlantRowEntityTable

---
+++ frontend/src/components/PlantRow/PlantRowEntityTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <EntityViewTable :dense="dense">
+    <EntityViewTableRow :label="t('entity.commonColumns.name')">
+      {{ plantRow.name }}
+    </EntityViewTableRow>
+    <EntityViewTableRow :label="t('plantRows.fields.orchard')">
+      <RouterLink
+        :to="`/orchards/${plantRow.orchard.id}`"
+        class="undecorated-link"
+      >
+        {{ plantRow.orchard.name }}
+      </RouterLink>
+    </EntityViewTableRow>
+    <EntityViewTableRow :label="t('plantRows.fields.dateCreated')">
+      {{ plantRow.date_created ? d(plantRow.date_created, 'Ymd') : '' }}
+    </EntityViewTableRow>
+    <EntityViewTableRow :label="t('entity.commonColumns.dateDisabled')">
+      {{ plantRow.date_eliminated ? d(plantRow.date_eliminated, 'Ymd') : '' }}
+    </EntityViewTableRow>
+    <EntityTableViewTimestampRows
+      :created="plantRow.created"
+      :modified="plantRow.modified"
+    />
+    <EntityViewTableRow :label="t('entity.commonColumns.note')" multiline>
+      {{ plantRow.note }}
+    </EntityViewTableRow>
+  </EntityViewTable>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'src/composables/useI18n';
+import { type PlantRowFragment } from './plantRowFragment';
+import EntityViewTable from 'src/components/Entity/View/EntityViewTable.vue';
+import EntityViewTableRow from 'src/components/Entity/View/EntityViewTableRow.vue';
+import EntityTableViewTimestampRows from 'src/components/Entity/View/EntityViewTableTimestampRows.vue';
+
+export interface PlantRowEntityTableProps {
+  plantRow: PlantRowFragment & {
+    note: string | null;
+  };
+  dense?: boolean | undefined;
+}
+
+defineProps<PlantRowEntityTableProps>();
+
+const { t, d } = useI18n();
+</script>

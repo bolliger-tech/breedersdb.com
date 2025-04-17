f5a41fc296846b0467b004ad67bca1f3fdcaf533
Cyrill Bolliger <mail@cyrill.me>
2025-04-17 18:08:30 +0200

    [UI] fix spacings in SignInPage

--- frontend/src/pages/Auth/SignInPage.vue
+++ frontend/src/pages/Auth/SignInPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div style="min-height: 100svh" class="column items-center justify-center">
     <div class="q-pa-md column items-center">
-      <h1 class="q-mb-lg">{{ orgName }}</h1>
+      <h1 class="q-mb-md">{{ orgName }}</h1>
       <h2>{{ t('auth.signInTitle') }}</h2>
       <SignInForm />
-      <div class="text-h4 text-muted">
+      <div class="text-h4 text-muted q-mt-xl">
         <BaseSpriteIcon name="blossom" size="lg" />
         <span style="margin-left: 8px; vertical-align: middle">
           {{ t('auth.signInFooter') }}

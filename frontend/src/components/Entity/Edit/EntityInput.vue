dee7097461a23b08ff6df199cb03a6cce43e944c
Cyrill Bolliger <mail@cyrill.me>
2025-04-17 17:56:42 +0200

    [REFACTOR] allow all QInputSlots on EntityInput and improve props handling

--- frontend/src/components/Entity/Edit/EntityInput.vue
+++ frontend/src/components/Entity/Edit/EntityInput.vue
@@ -9,12 +9,10 @@
       outlined
       :hint="hint ?? (required ? t('base.required') : '')"
       :clearable="!required"
-      :dark="$q.dark.isActive"
       :model-value="modelValue"
       :min="min"
       :max="max"
       :step="step"
-      :maxlength="maxlength"
       :pattern="pattern"
       :autocomplete="autocomplete"
       :placeholder="placeholder"
@@ -26,8 +24,12 @@
       @update:model-value="updateModelValue"
       @blur="blur"
     >
-      <template v-if="$slots.error" #error>
-        <slot name="error"></slot>
+      <template
+        v-for="slot in slotNames.filter((s) => s in $slots)"
+        :key="slot"
+        #[slot]
+      >
+        <slot :name="slot"></slot>
       </template>
     </q-input>
     <template v-if="$slots.explainer" #explainer>
@@ -38,27 +40,27 @@
 
 <script setup lang="ts">
 import { useInputBackground } from 'src/composables/useInputBackground';
-import type { QInput, QInputProps } from 'quasar';
+import type { QInput, QInputProps, QInputSlots } from 'quasar';
 import { nextTick, ref, type Slot } from 'vue';
 import { useI18n } from 'src/composables/useI18n';
 import BaseInputLabel from 'src/components/Base/BaseInputLabel.vue';
 import { focusInView } from 'src/utils/focusInView';
 
-export type EntityInputProps = Omit<
-  QInputProps,
-  'bgColor' | 'dense' | 'outlined' | 'modelValue'
-> & {
+export interface EntityInputProps
+  extends Omit<
+    QInputProps,
+    'bgColor' | 'dense' | 'outlined' | 'modelValue' | 'dark' // if the `dark` prop is not excluded, the colors are wrong in dark mode
+  > {
   required?: boolean | undefined;
   min?: number | string | undefined;
   max?: number | string | undefined;
   step?: number | undefined;
   pattern?: string | undefined;
-  maxlength?: number | undefined;
   autocomplete?: string | undefined;
   explainer?: string | undefined;
   placeholder?: string | undefined;
   trim?: boolean | undefined; // workaround for https://github.com/quasarframework/quasar/issues/17663
-};
+}
 
 const props = defineProps<EntityInputProps>();
 
@@ -70,7 +72,11 @@ defineExpose({
 
 const modelValue = defineModel<QInputProps['modelValue']>();
 
-defineSlots<{ error: Slot; explainer: Slot }>();
+export interface EntityInputSlots extends QInputSlots {
+  explainer: Slot;
+}
+const slots = defineSlots<EntityInputSlots>();
+const slotNames = Object.keys(slots) as (keyof typeof slots)[];
 
 function updateModelValue(value: QInputProps['modelValue']) {
   if (!props.required && value === '') {

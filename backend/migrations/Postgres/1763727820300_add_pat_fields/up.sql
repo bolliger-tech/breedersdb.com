ee5e657f97854ea6b9fd5582858c4e6e668eb411
Cyrill Bolliger <mail@cyrill.me>
2025-11-21 15:23:10 +0100

    [FEAT] allow PAT creation

--- backend/migrations/Postgres/1763727820300_add_pat_fields/up.sql
+++ backend/migrations/Postgres/1763727820300_add_pat_fields/up.sql
@@ -1,5 +1,11 @@
+alter table user_tokens drop constraint user_tokens_type_check;
+alter table user_tokens add constraint user_tokens_type_check
+            check ((type)::text = ANY (ARRAY [('cookie'::character varying)::text, ('pat'::character varying)::text]));
+
 alter table user_tokens
-    add column name    citext unique
+    add column name    citext
         check ( (type = 'pat' and name is not null and name ~ '^[^/\n]{1,120}$') or (type != 'pat' and name is null) ),
     add column expires timestamp with time zone
-        check (type != 'pat' and expires is null);
+        check (type = 'pat' or (type != 'pat' and expires is null));
+
+create unique index on user_tokens (user_id, name);

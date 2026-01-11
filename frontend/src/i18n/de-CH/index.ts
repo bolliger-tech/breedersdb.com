a268bba8a440da3c9b483caf2ec4cc3d32fe51d2
cyrillbolliger <12530490+cyrillbolliger@users.noreply.github.com>
2026-01-11 20:11:25 +0000

    Latest translations from Crowdin

--- frontend/src/i18n/de-CH/index.ts
+++ frontend/src/i18n/de-CH/index.ts
@@ -25,6 +25,7 @@ import { analyze } from './analyze';
 import { pwa } from './pwa';
 import { settings } from './settings';
 import { personalAccessTokens } from './personal-access-tokens';
+import { print } from './print';
 
 const messages = {
   base,
@@ -34,6 +35,7 @@ const messages = {
   pwa,
   settings,
   personalAccessTokens,
+  print,
 
   entity,
   attributes,
